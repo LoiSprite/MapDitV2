33d7e1fdbf64042021f3cf3340b94db36d8bdd2d
LoiSprite <159018964+LoiSprite@users.noreply.github.com>
2026-06-14 22:14:08 +0700

    Update AntiAfk.lua

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -1,8 +1,1 @@
-local VirtualInputManager = game:GetService("VirtualInputManager")
-while true do
-    wait(60)
-    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.K, false, game)
-    wait(0.1)
-    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.K, false, game)
-    print("concac")
-end
+
