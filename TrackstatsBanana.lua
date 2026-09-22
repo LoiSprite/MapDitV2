304bace2de45ded61e2933f85e18a47c03f7f388
LoiSprite <159018964+LoiSprite@users.noreply.github.com>
2026-09-22 17:56:23 +0700

    Update TrackstatsBanana.lua

--- TrackstatsBanana.lua
+++ TrackstatsBanana.lua
@@ -1,14 +1,22 @@
-kaitun getgenv().AnimeDiceAutoChangeConfig = {
-	Enabled  = true,
-	Interval = 3,
-	Item   = "Jackpot Spin",
-	Amount = 1,
-	FromFolder = "5178455e59d2d46de062cdc3349eaf0abdc0f5e7579b0bdb3a5d617badece1bf",           -- ID folder acc chính
-	ToFolder   = "2bc83f3a30d9bce6d7cc71df2d7acc1f7d5c2cbc488bc6b95fb51b9ba5e816c2",           -- ID folder acc thay thế
-	Replace = false,           -- tham số 3 của ChangeToFolder
-	RetryOnFail = true,
-	Log = true,
-	UI = { Enabled = true, ToggleKey = Enum.KeyCode.RightAlt },
+getgenv().AnimeDiceAutoChangeConfig = {
+    Enabled  = true,
+    Interval = 3,
+    Item   = "Jackpot Spin",
+    Amount = 1,
+    Backend = "api",
+    Api = {
+        Url = "https://accountops.org",
+        Key = "ak_d5001c83bb5fe8ecd64423c496c3c3aed4b6daf19c1ebd98d42cee3f07c83bf6",
+        AuthMode = "x-api-key",
+        Username = "loisprite",
+        Option   = 1,
+        RetryEvery = 60,
+    },
+    FromFolder = "",
+    ToFolder   = "",
+    Replace    = false,
+    RetryOnFail = true,
+    Log = true,
+    UI = { Enabled = true, ToggleKey = Enum.KeyCode.RightAlt },
 }
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/quoc12092008/Chuiroblox2/refs/heads/main/change.lua"))()
